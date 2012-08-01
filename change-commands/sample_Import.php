490b2f590c3010061325e877b35537127f064291
Renaud JENNY <renaud.jenny@rbs.fr>
2012-08-01 16:01:13 +0200

    [UPDATE] Always preparing the new sample format

--- change-commands/sample_Import.php
+++ change-commands/sample_Import.php
@@ -6,14 +6,16 @@
 class commands_sample_Import extends commands_AbstractChangeCommand
 {
 	/**
+	 *
 	 * @return String
 	 */
 	public function getUsage()
 	{
-		return "<cmscore|ecommercecore|cmsecomos|ecom-performance|'moduleName/sample.xml'>";
+		return "<core|cms|ecommercecms>";
 	}
-
+	
 	/**
+	 *
 	 * @return String
 	 */
 	public function getDescription()
@@ -23,44 +25,47 @@ class commands_sample_Import extends commands_AbstractChangeCommand
 	
 	/**
 	 * This method is used to handle auto-completion for this command.
-	 * @param Integer $completeParamCount the parameters that are already complete in the command line
-	 * @param String[] $params
-	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
+	 *
+	 * @param Integer $completeParamCount
+	 *        	the parameters that are already complete in the command line
+	 * @param String[] $params        	
+	 * @param
+	 *        	array<String, String> $options where the option array key is
+	 *        	the option name, the potential option value or true
 	 * @return String[] or null
 	 */
 	public function getParameters($completeParamCount, $params, $options, $current)
 	{
-		$components = array_merge(array('cmscore', 'ecommercecore', 'cmsecomos', 'ecom-performance', 'all'), $this->getSamples());
+		$components = array('core', 'cms', 'ecommercecms');
 		
 		return $components;
 	}
 	
 	/**
-	 * @param String[] $params
-	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
+	 *
+	 * @param String[] $params        	
+	 * @param
+	 *        	array<String, String> $options where the option array key is
+	 *        	the option name, the potential option value or true
 	 * @return boolean
 	 */
 	protected function validateArgs($params, $options)
 	{
-		return (count($params) == 1 && in_array($params[0], array_merge(array('cmscore', 'ecommercecore', 'cmsecomos', 'ecom-performance', 'all'), $this->getSamples())));
+		return (count($params) == 1 && in_array($params[0], array('core', 'cms', 'ecommercecms')));
 	}
-
+	
 	/**
-	 * @return String[]
-	 */
-//	public function getOptions()
-//	{
-//	}
-
-	/**
-	 * @param String[] $params
-	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
+	 *
+	 * @param String[] $params        	
+	 * @param
+	 *        	array<String, String> $options where the option array key is
+	 *        	the option name, the potential option value or true
 	 * @see c_ChangescriptCommand::parseArgs($args)
 	 */
 	public function _execute($params, $options)
 	{
 		$this->message("== Initdata ==");
-
+		
 		$this->loadFramework();
 		
 		$parent = $this->getParent();
@@ -68,37 +73,30 @@ class commands_sample_Import extends commands_AbstractChangeCommand
 		$parent->executeCommand('theme.install');
 		$batchPath = 'modules/sample/lib/bin/batchImport.php';
 		
-		$ecomMode = true;
+		// Update config
+		$this->updateConfigXML();
+		$parent->executeCommand('compile-config');
 		
 		switch ($params[0])
 		{
-			case 'cmscore':
+			case 'core' :
 				$this->message('= Import samples of your OS Core modules =');
 				$wantedSamples = $this->getOSCoreSamples();
-				$ecomMode = false;
 				break;
-			case 'ecommercecore':
-				$this->message('= Import samples of your OS Ecom & Core modules =');
+			case 'cms' :
+				$this->message('= Import samples of CMS & Core modules =');
+				$wantedSamples = $this->getCMSSamples();
+				break;
+			case 'ecommercecms' :
+				$this->message('= Import samples of your OS Ecom & Core modules	=');
 				$wantedSamples = $this->getOSEcomSamples();
 				break;
-			case 'cmsecomos':
-					$this->message('= Import samples of all OS modules =');
-					$wantedSamples = $this->getCMSEcomOSSamples();
-					break;
-			case 'ecom-performance':
-				$this->message('= Import samples of your OS Core, Ecom & Ecom performance modules =');
-				$wantedSamples = $this->getEcomPerformanceSamples();
-				break;
-			case 'all':
-				$this->message('= Import samples of all your modules =');
-				$wantedSamples = $this->getSamples();
-				break;
-			default:
+			default :
 				$this->message('= Import only ' . $params[0] . ' =');
 				$wantedSamples = array($params[0]);
 		}
 		
-		$samples = array_values(array_intersect($this->getSamples(), $wantedSamples));
+		$samples = $wantedSamples;
 		
 		$ms = ModuleService::getInstance();
 		
@@ -124,189 +122,100 @@ class commands_sample_Import extends commands_AbstractChangeCommand
 			}
 			$index++;
 		}
-		$parent->executeCommand('i18n-synchro');
-		if ($ecomMode && $ms->isInstalled('catalog'))
+		if ($ms->isInstalled('catalog'))
 		{
 			if ($ms->isInstalled('mysqlstock'))
 			{
 				$parent->executeCommand('mysqlstock.reset-products-stock');
 			}
 			$parent->executeCommand('catalog.compile-catalog');
-			
-			$this->message("Replace default page templates by eCom page templates");
-			$tps = theme_PagetemplateService::getInstance();
-			$tps->replacePagetemplate($tps->getByCodeName('default/nosidebarpage'), $tps->getByCodeName('default/nosidebarpage-ecom'));
-			$tps->replacePagetemplate($tps->getByCodeName('default/sidebarpage'), $tps->getByCodeName('default/sidebarpage-ecom'));
 		}
+		
 		$parent->executeCommand('enable-site');
 		
 		$this->quitOk("Command successfully executed");
 	}
 	
 	/**
-	 * All samples of all modules ordered
-	 * @return string[]
+	 * update project.xml
 	 */
-	private function getSamples()
+	public function updateConfigXML()
 	{
-		//This method preserves the good order of samples. 
-		//It return the ordered array to compare with other 
-		//method that return an unordered array of samples you want
+		$file = WEBEDIT_HOME . '/config/project.xml';
 		
-		$samples = array();
+		$doc = f_util_DOMUtils::fromPath($file);
 		
-		$samples[] = 'website' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'media' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'photoalbum' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'users' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'workflow' . DIRECTORY_SEPARATOR . 'sample.xml';
+		$nodeWebsite = $doc->findUnique('//project/config/modules/website');
 		
-		$samples[] = 'emailing' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'emailing' . DIRECTORY_SEPARATOR . 'samplepage.xml';
+		if ($nodeWebsite == NULL)
+		{
+			$nodeModules = $doc->findUnique('//project/config/modules');
+			$newNode = $doc->createElement("website");
+			$nodeModules->appendChild($newNode);
+		}
 		
-		$samples[] = 'forums' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'blog' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'form' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'download' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'notification' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'contactcard' . DIRECTORY_SEPARATOR . 'samplecontactcard.xml';
-		$samples[] = 'list' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'faq' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'lexicon' . DIRECTORY_SEPARATOR . 'sample.xml';
+		$nodeSample = $doc->findUnique('//project/config/modules/website/sample');
 		
-		$samples[] = 'survey' . DIRECTORY_SEPARATOR . 'sample.xml';
+		if ($nodeSample == NULL)
+		{
+			$nodeWebsite = $doc->findUnique('//project/config/modules/website');
+			$newNode = $doc->createElement("sample");
+			$nodeWebsite->appendChild($newNode);
+			
+			$nodeSample = $doc->findUnique('//project/config/modules/website/sample');
+			
+			$nameTemplate = array('defaultPageTemplate' => 'default/sidebarpageecomsample', 
+				'defaultNosidebarTemplate' => 'default/nosidebarpageecomsample', 'defaultHomeTemplate' => 'default/nosidebarpageecomsample');
+			
+			foreach ($nameTemplate as $key => $value)
+			{
+				$newTextNode = $doc->createTextNode($value);
+				
+				$newNode = $doc->createElement("entry");
+				$newNode->setAttribute('name', $key);
+				$newNode->appendChild($newTextNode);
+				
+				$nodeSample->appendChild($newNode);
+			}
+		}
 		
-		$samples[] = 'inquiry' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'joboffer' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'sharethis' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'ads' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'videos' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'polls' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'statictext' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'brand' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'shipping' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'payment' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'atosserver' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'paybox' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'catalog' . DIRECTORY_SEPARATOR . 'samplecatalog.xml';
-		$samples[] = 'catalog' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'customer' . DIRECTORY_SEPARATOR . 'default.xml';
-		$samples[] = 'order' . DIRECTORY_SEPARATOR . 'default.xml';
-		$samples[] = 'customer' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'loyalty' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'marketing' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'order' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'privatesales' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'productreturns' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'event' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'rss' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'bookmarks' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'privatemessaging' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'mobileapps' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'store' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'store' . DIRECTORY_SEPARATOR . 'sampleextranet.xml';
-		$samples[] = 'synchronizer' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		return $samples;
+		$doc->save($file);
 	}
 	
+	
+	
 	/**
-	 * Samples of OS CMS Core pack unordered
 	 * @return string[]
 	 */
 	private function getOSCoreSamples()
 	{
 		$samples = array();
 		
-		$samples[] = 'website' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'media' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'users' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'workflow' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'form' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'notification' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'contactcard' . DIRECTORY_SEPARATOR . 'samplecontactcard.xml';
-		$samples[] = 'list' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'statictext' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'rss' . DIRECTORY_SEPARATOR . 'sample.xml';
+		$samples[] = 'sample' . DIRECTORY_SEPARATOR . 'samplecore.xml';
 		
 		return $samples;
 	}
 	
 	/**
-	 * Samples of OS E-commerce core pack unordered
 	 * @return string[]
 	 */
 	private function getOSEcomSamples()
 	{
 		$samples = $this->getOSCoreSamples();
 		
-		$samples[] = 'brand' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'shipping' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'payment' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'paybox' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'catalog' . DIRECTORY_SEPARATOR . 'samplecatalog.xml';
-		$samples[] = 'catalog' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'customer' . DIRECTORY_SEPARATOR . 'default.xml';
-		$samples[] = 'order' . DIRECTORY_SEPARATOR . 'default.xml';
-		$samples[] = 'customer' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'order' . DIRECTORY_SEPARATOR . 'sample.xml';
+		$samples[] = 'sample' . DIRECTORY_SEPARATOR . 'sampleecommerce.xml';
 		
 		return $samples;
 	}
 	
 	/**
-	 * All OS samples unordered
 	 * @return string[]
 	 */
-	private function getCMSEcomOSSamples()
+	private function getCMSSamples()
 	{
-		$samples = $this->getOSEcomSamples();
+		$samples = $this->getOSCoreSamples();
 		
-		$samples[] = 'forums' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'blog' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'download' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'faq' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'lexicon' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'inquiry' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'joboffer' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'sharethis' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'videos' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'polls' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'event' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'bookmarks' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'privatemessaging' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		return $samples;
-	}
-	
-	/**
-	 * Samples of OS E-commerce performance pack unordered
-	 * @return string[]
-	 */
-	private function getEcomPerformanceSamples()
-	{
-		$samples = $this->getOSEcomSamples();
-		
-		$samples[] = 'emailing' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'emailing' . DIRECTORY_SEPARATOR . 'samplepage.xml';
-		
-		$samples[] = 'ads' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'loyalty' . DIRECTORY_SEPARATOR . 'sample.xml';
-		$samples[] = 'marketing' . DIRECTORY_SEPARATOR . 'sample.xml';
-		
-		$samples[] = 'productreturns' . DIRECTORY_SEPARATOR . 'sample.xml';
+		$samples[] = 'sample' . DIRECTORY_SEPARATOR . 'samplecms.xml';
 		
 		return $samples;
 	}
